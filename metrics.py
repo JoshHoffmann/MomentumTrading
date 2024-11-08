Fixed an issue with returns calculating. pct_change calculation now correctly calculates pct change between rebalancing points only and accounts for signals at these points.

metrics.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 import plotting
+pd.set_option('display.max_rows', None)  # Show all rows
+pd.set_option('display.max_columns', None)
 
 def priceRebalance(priceData,rebalancePeriod):
     rebalancePoints = priceData.resample(rebalancePeriod).last().index
@@ -9,30 +11,27 @@ def priceRebalance(priceData,rebalancePeriod):
 
     w_rebalanced = priceData.loc[adj_rebalancePoints].reindex(priceData.index).ffill().dropna()
 
-    return w_rebalanced
+    return w_rebalanced, adj_rebalancePoints
 
-def Returns(priceData:pd.DataFrame,signal:pd.DataFrame,rebalancePeriod:str):
+def Returns(priceData:pd.DataFrame,signal:pd.DataFrame,rebalancePeriod:str,t=True):
 
-    prices_rebalanced = priceRebalance(priceData,rebalancePeriod).loc[signal.index,:]
+    prices_rebalanced, rebalancePoints = priceRebalance(priceData.loc[signal.index],rebalancePeriod)
 
-    pct = prices_rebalanced.pct_change()
-    print('PCT')
-    print(pct.head())
-    print('SIGNAL')
-    print(signal.head())
-    returns = (signal * pct.dropna()).sum(axis=1)
-    returns.columns=['Returns']
-    plotting.plotReturns(returns)
+    pct = prices_rebalanced.loc[rebalancePoints].pct_change().fillna(0)
+
+    returns = (pct * signal.loc[rebalancePoints]).sum(axis=1)
+
     return returns
 
 def CumulativeReturns(priceData:pd.DataFrame,signal:pd.DataFrame, rebalancePeriod):
-    returns = Returns(priceData,signal,rebalancePeriod)
+    returns = Returns(priceData,signal,rebalancePeriod,t=False)
     cumulativeReturns = (1+returns).cumprod()-1
     cumulativeReturns.columns = ['CumulativeReturns']
     plotting.plotCumulative(cumulativeReturns)
     return cumulativeReturns
 
 def Sharpe(priceData:pd.DataFrame,signal:pd.DataFrame,rebalancePeriod):
-    returns = Returns(priceData,signal,rebalancePeriod)
+    returns = Returns(priceData,signal,rebalancePeriod,t=False)
     Sharpe = np.round(returns.mean(axis=0) / returns.std(axis=0), 2)
     return Sharpe
+