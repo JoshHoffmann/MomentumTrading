63a539d9a9a78793300989cacf382e66ecdb8061
JoshH <joshhoffmann@outlook.com>
2024-11-11 18:04:08 +0000

    Added volAdjust weighting function which will weight signals inversely proprtional to their rolling returns volatility over a selected window (defaults to 33) Added volProp weighting function which weights proportionally to rolling volatility

--- weighting.py
+++ weighting.py
@@ -3,12 +3,13 @@ import numpy as np
 
 
 class signalWeighter:
-    def __init__(self, unweighted:pd.DataFrame,z:pd.DataFrame,momenta:pd.DataFrame):
+    def __init__(self, unweighted:pd.DataFrame,z:pd.DataFrame,momenta:pd.DataFrame,priceData:pd.DataFrame):
         self.unweighted = unweighted
         self.z = z
         self.momenta = momenta
+        self.priceData = priceData
 
-    def linear(self,beta):
+    def linear(self,beta:float)->pd.DataFrame:
         """Weight signals linearly in proportion to their z-score"""
         print('BETA = ', beta)
         I = self.unweighted.index
@@ -26,15 +27,23 @@ class signalWeighter:
 
         return weighted
 
-    def volAdjust(self,priceData:pd.DataFrame,window:int):
+    def volAdjust(self,window:int)->pd.DataFrame:
         I = self.unweighted.index
         momenta_sign = self.momenta.loc[I, :].apply(np.sign)
-        vol = priceData.pct_change().loc[I,:].rolling(window=window).std()
+        vol = self.priceData.pct_change().loc[I,:].rolling(window=window).std()
         weighted = self.unweighted*self.z.abs()*momenta_sign/vol
         weighted = weighted.div(weighted.abs().sum(axis=1), axis=0)
         return weighted
 
-    def getWeightedSignal(self, function, params):
+    def volProp(self,window:int)->pd.DataFrame:
+        I = self.unweighted.index
+        momenta_sign = self.momenta.loc[I, :].apply(np.sign)
+        vol = self.priceData.pct_change().loc[I, :].rolling(window=window).std()
+        weighted = self.unweighted * self.z.abs() * momenta_sign * vol
+        weighted = weighted.div(weighted.abs().sum(axis=1), axis=0)
+        return weighted
+
+    def getWeightedSignal(self, function:str, params:dict)->pd.DataFrame:
         if function == 'linear':
             beta = params.get('beta',1)
             return self.linear(beta)
@@ -44,4 +53,8 @@ class signalWeighter:
         elif function == 'volAdjust':
             priceData = params.get('priceData')
             window = params.get('window',33)
-            return self.volAdjust(priceData,window)
+            return self.volAdjust(window)
+        elif function=='volProp':
+            priceData = params.get('priceData')
+            window = params.get('window',33)
+            return self.volProp(window)
